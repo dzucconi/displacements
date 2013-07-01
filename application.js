49d540a3f493558d352295f0d0695eab334b60bd
dzucconi <mail@damonzucconi.com>
2013-07-01 11:57:18 -0400

    Implements a function that mirrors the video image on every other frame

--- application.js
+++ application.js
@@ -1,3 +1,29 @@
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         function(callback) { window.setTimeout(callback, 1000 / 60); };
+})();
+
 (function(){
   "use strict";
+
+  $(function(){
+    var $video = $("#video");
+
+    $video.on("play", function() {
+      var i = 0;
+
+      (function animloop() {
+        requestAnimFrame(animloop);
+
+        var val = -1;
+        if (i % 2 === 0) { val = Math.abs(val) };
+
+        $video.css({"-webkit-transform": "scaleX(" + val + ")", "transform": "scaleX(" + val + ")"});
+
+        i++;
+      }());
+    });
+  });
 }());
