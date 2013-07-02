bfe244a9d64a8c17341ee9be2ffb46ef46ddd20b
dzucconi <mail@damonzucconi.com>
2013-07-02 00:18:54 -0400

    Cover all vendor prefixes

--- application.js
+++ application.js
@@ -8,7 +8,15 @@ window.requestAnimFrame = (function() {
 $(function() {
   "use strict";
 
-  var $video = $("#video");
+  var $video = $("#video"),
+
+      vendored = function(factor) {
+        return ["-webkit-", "-moz-", "-ms-", "-o-", ""].reduce(function(obj, prefix) {
+          obj[prefix + "transform"] = "scaleX(" + factor + ")";
+
+          return obj;
+        }, {});
+      }
 
   $video.on("play", function() {
     var i = 0, factor;
@@ -16,7 +24,7 @@ $(function() {
     (function run() {
       if (i % 2 === 0) { factor = -1; } else { factor = 1 }; i++;
 
-      $video.css({ "-webkit-transform": "scaleX(" + factor + ")", "transform": "scaleX(" + factor + ")" });
+      $video.css(vendored(factor));
 
       requestAnimFrame(run);
     }());
