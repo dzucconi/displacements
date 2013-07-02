9ddcd64802ee47d04e1defed0b97268a2d9278c5
dzucconi <mail@damonzucconi.com>
2013-07-02 00:13:05 -0400

    Remove outer closure; slight naming clarification

--- application.js
+++ application.js
@@ -5,24 +5,20 @@ window.requestAnimFrame = (function() {
          function(callback) { window.setTimeout(callback, 1000 / 60); };
 })();
 
-(function(){
+$(function() {
   "use strict";
 
-  $(function(){
-    var $video = $("#video");
+  var $video = $("#video");
 
-    $video.on("play", function() {
-      var i = 0, val;
+  $video.on("play", function() {
+    var i = 0, factor;
 
-      (function run() {
-        if (i % 2 === 0) { val = -1; } else { val = 1 };
+    (function run() {
+      if (i % 2 === 0) { factor = -1; } else { factor = 1 }; i++;
 
-        $video.css({ "-webkit-transform": "scaleX(" + val + ")", "transform": "scaleX(" + val + ")" });
+      $video.css({ "-webkit-transform": "scaleX(" + factor + ")", "transform": "scaleX(" + factor + ")" });
 
-        i++;
-
-        requestAnimFrame(run);
-      }());
-    });
+      requestAnimFrame(run);
+    }());
   });
-}());
+});
