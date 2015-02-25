790aa3bb7b657ec23c15c618886b80d3054a0967
dzucconi <mail@damonzucconi.com>
2015-02-25 10:13:51 -0500

    Ok.

--- application.js
+++ application.js
@@ -10,23 +10,21 @@ $(function() {
 
   var $video = $("#video"),
 
-      vendored = function(factor) {
-        return ["-webkit-", "-moz-", "-ms-", "-o-", ""].reduce(function(obj, prefix) {
-          obj[prefix + "transform"] = "scaleX(" + factor + ")";
+  vendored = function(factor) {
+    return ["-webkit-", "-moz-", "-ms-", "-o-", ""].reduce(function(obj, prefix) {
+      obj[prefix + "transform"] = "scaleX(" + factor + ")";
 
-          return obj;
-        }, {});
-      }
+      return obj;
+    }, {});
+  }
 
-  $video.on("play", function() {
-    var i = 0, factor;
+  var i = 0, factor;
 
-    (function run() {
-      if (i % 2 === 0) { factor = -1; } else { factor = 1 }; i++;
+  (function run() {
+    if (i % 2 === 0) { factor = -1; } else { factor = 1 }; i++;
 
-      $video.css(vendored(factor));
+    $video.css(vendored(factor));
 
-      requestAnimFrame(run);
-    }());
-  });
+    requestAnimFrame(run);
+  }());
 });
